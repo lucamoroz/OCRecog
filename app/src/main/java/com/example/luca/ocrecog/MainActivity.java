a52d00eaa0bf911a9e47394e24d65d11f8895790
lucamoroz <44212562+lucamoroz@users.noreply.github.com>
2018-10-26 20:30:16 +0200

    added tempfile delete

--- app/src/main/java/com/example/luca/ocrecog/MainActivity.java
+++ app/src/main/java/com/example/luca/ocrecog/MainActivity.java
@@ -1,25 +1,21 @@
 package com.example.luca.ocrecog;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,6 +28,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import static android.app.PendingIntent.getActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     String mCurrentPhotoPath;
@@ -41,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
     private Button takePictureButton;
     private TextView showTextView;
     private ImageView photoImageView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,9 +57,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        loadImageFromStorage("/data/user/0/com.example.luca.ocrecog/app_imageDir");
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String pathToImageInternalStorage = sharedPref.getString(getString(R.string.savedPhotoPathKey), "");
+
+        if(pathToImageInternalStorage != "")
+            loadImageFromStorage(pathToImageInternalStorage);
     }
 
+
+    //Launch an intent to take a picture
+    //Stores the image to external memory and save path in mCurrentPhotoPath
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
@@ -80,9 +84,11 @@ public class MainActivity extends AppCompatActivity {
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                photoFile.delete();
             }
         }
     }
+
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -99,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+    //Show image on activity and save it in internal storage as bitmap, modifies photoPath in shared preference
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -111,7 +118,15 @@ public class MainActivity extends AppCompatActivity {
             bitmap = Bitmap.createBitmap(bitmap , 0, 0, bitmap .getWidth(), bitmap .getHeight(), matrix, true);
 
             photoImageView.setImageBitmap(bitmap);
-            saveToInternalStorage(bitmap);
+
+            String imagePathInternalStorage = saveToInternalStorage(bitmap);
+            SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString(getString(R.string.savedPhotoPathKey), imagePathInternalStorage);
+            editor.apply();
+
+
+            
         }
 
     }
@@ -133,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         } finally {
             try {
+                assert fos != null;
                 fos.close();
             } catch (IOException e) {
                 e.printStackTrace();
