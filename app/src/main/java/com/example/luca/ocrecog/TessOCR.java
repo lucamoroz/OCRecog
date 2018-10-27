7dd076bea9ef3746414a98c0e52156e2a41f2df2
lucamoro <lucamoro96@gmail.com>
2018-10-27 21:41:38 +0200

    added text filter based on words confidence

--- app/src/main/java/com/example/luca/ocrecog/TessOCR.java
+++ app/src/main/java/com/example/luca/ocrecog/TessOCR.java
@@ -1,6 +1,7 @@
 package com.example.luca.ocrecog;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
@@ -9,8 +10,10 @@ public class TessOCR implements OCRWrapper {
     public static final String ENG = "eng";
     private String mDataPath;
     private String mLanguage;
+    private int minConfidence = 60;
 
     TessBaseAPI tessBaseAPI;
+
     public TessOCR(String dataPath, String language) {
         mDataPath = dataPath;
         mLanguage = language;
@@ -26,17 +29,55 @@ public class TessOCR implements OCRWrapper {
         tessBaseAPI = new TessBaseAPI();
         tessBaseAPI.init(mDataPath, mLanguage);
         tessBaseAPI.setImage(img);
-        String recogText = tessBaseAPI.getUTF8Text();
+        String text = tessBaseAPI.getUTF8Text();
+
+        int[] wordsConfidence = tessBaseAPI.wordConfidences();
+        text = filterText(text, wordsConfidence, this.minConfidence);
+
         tessBaseAPI.end();
-        return recogText;
+        return text;
     }
     public String getTextWithConfidenceFromImg(Bitmap img) {
         tessBaseAPI = new TessBaseAPI();
         tessBaseAPI.init(mDataPath, mLanguage);
         tessBaseAPI.setImage(img);
-        String recogText = tessBaseAPI.getUTF8Text();
+        String text = tessBaseAPI.getUTF8Text();
+
+        int[] wordsConfidence = tessBaseAPI.wordConfidences();
+        text = filterText(text, wordsConfidence, this.minConfidence);
+
         int meanConfidence = tessBaseAPI.meanConfidence();
         tessBaseAPI.end();
-        return recogText + "\n" + "Mean confidence: " + meanConfidence;
+        return text + "\n" + "Mean confidence: " + meanConfidence;
     }
+
+    public void setMinConfidence(int minConfidence) {
+        this.minConfidence = minConfidence;
+    }
+
+    private String filterText(String text, int[] wordsConfidence, int minConfidence) {
+        String[] words = text.split(" ");
+
+        if(words.length != wordsConfidence.length) {
+            Log.i("TextFilter", "Error filtering text: lenghts don't correspond. N words: " + Integer.toString(words.length) + " N confidences: " + Integer.toString(wordsConfidence.length));
+        }
+
+
+        String filteredText = "";
+
+        int count = Math.min(words.length, wordsConfidence.length);
+
+        for(int i = 0; i < count; i++) {
+            if(wordsConfidence[i] > minConfidence)
+                filteredText += words[i] + " ";
+        }
+
+        if(count < words.length) {
+            for(int i = count; i < words.length; i++)
+                filteredText += words[i] + " ";
+        }
+
+        return filteredText;
+    }
+
 }
